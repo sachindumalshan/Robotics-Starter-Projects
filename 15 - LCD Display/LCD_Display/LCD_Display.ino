fe5b89f67f5658991734f985040d1f24223368ea
Sachindu Malshan <sachindu.info@gmail.com>
2023-08-25 20:50:26 +0530

    code changed

--- 15 - LCD Display/LCD_Display/LCD_Display.ino
+++ 15 - LCD Display/LCD_Display/LCD_Display.ino
@@ -12,9 +12,9 @@ void loop()
 {
   lcd.clear();                 // clear display
   lcd.setCursor(0, 0);         // move cursor to   (0, 0)
-  lcd.print("Arduino");        // print message at (0, 0)
+  lcd.print("Welcome");        // print message at (0, 0)
   lcd.setCursor(2, 1);         // move cursor to   (2, 1)
-  lcd.print("GetStarted.com"); // print message at (2, 1)
+  lcd.print("Arduino"); // print message at (2, 1)
   delay(2000);                 // display the above for two seconds
 
   lcd.clear();                  // clear display
